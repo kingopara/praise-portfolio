6e2b33e5440c01d0f562f6be8c1256ee8dfe862e
praiseopara <cpopara2010@yahoo.com>
2021-10-23 07:04:20 -0500

    working on about section

--- src/components/Pages/About.js
+++ src/components/Pages/About.js
@@ -5,7 +5,9 @@ function About() {
     return (
         <div className='about'>
             <div className='about-text'>
-                I'm a Full-Stack Software Engineer with a background in Petroleum Engineering working currently in the (photomask) manufacturing industry. I see Web-development together with Engineering as the closiest thing to magic and thats what caught my attention and interest.
+                I'm a Full-Stack Software Engineer with a background in Petroleum Engineering working currently in the (photomask) manufacturing industry.
+                <br/>
+                I see Web-development together with Engineering as the closiest thing to magic and thats what caught my attention and interest.
                 <br/>
                 Come work with me lets make some MAGIC!
             </div>
