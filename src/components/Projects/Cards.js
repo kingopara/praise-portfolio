fa85fe2dcbf6fcc02578f4c83615cda05f811608
praiseopara <cpopara2010@yahoo.com>
2021-12-03 07:37:27 -0600

    updating portfolio

--- src/components/Projects/Cards.js
+++ src/components/Projects/Cards.js
@@ -25,6 +25,24 @@ function Cards({project}) {
                     )}
                 </div>
                 <p>{project.description}</p>
+                <p className="tech-used">Technologies Used</p>
+                <div className="tech">
+                    <div className="project-tech">
+                        {project.tag1}
+                    </div>
+                    <div className="project-tech">
+                        {project.tag2}
+                    </div>
+                    <div className="project-tech">
+                        {project.tag3}
+                    </div>
+                    <div className="project-tech">
+                        {project.tag4}
+                    </div>
+                    <div className="project-tech">
+                        {project.tag5}
+                    </div>
+                </div>
             </div>
             <img src={project.image} className='project-img' alt="screenshot"></img>
         </div>
--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,19 +1,42 @@
 export const Projects = [
     {
+        id: 1,
+        title: "Book MarketPlace",
+        description: "A search engine that helps avid readers to search for books to read and keep track of a list of books to purchase.",
+        deployedApp: "https://book-search-engine-app21.herokuapp.com",
+        github: "https://github.com/kingopara/book-search-engine",
+        tag1: "react",
+        tag2: "express",
+        tag3: "graphql",
+        tag4: "bootstrap",
+        tag5: "concurrently",
+        image: require("../../assets/Projects/preview copy.png").default
+    },
+    {
         id:1,
         title: "Deep-Thoughts",
         description: "A social media app which enables users to signup and log into personal accounts, express their thoughts and concerns, add friends and also react to friends thoughts",
         deployedApp: "https://deep-thoughts-app-21.herokuapp.com/",
         github: "https://github.com/kingopara/deep-thoughts",
+        tag1: "node.js",
+        tag2: "express.js",
+        tag3: "graphql",
+        tag4: "react",
+        tag5: "mongoDB",
         image: require("../../assets/Projects/deep-thoughts.png").default
     },
     {
         id:1,
-        title: "Tech Blog",
-        description: "This app is one of a kind. This CMS-style blog site was created for developers who write about Technology to be able to publish articles, blog posts, thoughts and opinions.",
-        deployedApp: "https://tech-blog-app21.herokuapp.com/",
-        github: "https://github.com/kingopara/tech-blog",
-        image: require("../../assets/Projects/techblog.png").default
+        title: "Password Generator",
+        description: "As a user with access to sensitive data, this web application helps to randomly generate a password that meets certain criteria so that users can create a strong password that provides greater security.",
+        deployedApp: "https://kingopara.github.io/PasswordGenerator./",
+        github: "https://github.com/kingopara/PasswordGenerator.",
+        tag1: "HTML",
+        tag2: "CSS",
+        tag3: "JavaScript",
+        tag4: "VS Code",
+        tag5: "git",
+        image: require("../../assets/Projects/preview copy 2.png").default
     },
     {
         id:1,
@@ -21,6 +44,11 @@ export const Projects = [
         description: "Pizza hunt is a web application that enables users to create their own pizzas from a selection of toppings and also custom toppings made by them.",
         deployedApp: "https://pizza-hunt-app21.herokuapp.com/",
         github: "https://github.com/kingopara/pizza-hut",
+        tag1: "node.js",
+        tag2: "express.js",
+        tag3: "mongoose",
+        tag4: "mongoDB",
+        tag5: "indexedDB",
         image: require("../../assets/Projects/pizza-hunt.png").default
     },
     {
@@ -29,6 +57,11 @@ export const Projects = [
         description: "This note taker web application helps users to write and save notes so as to keep track of tasks that need to be completed and also organize their thoughts.",
         deployedApp: "https://note-taker-28.herokuapp.com/",
         github: "https://github.com/kingopara/note-taker",
+        tag1: "node.js",
+        tag2: "express.js",
+        tag3: "JavaScript",
+        tag4: "HTML",
+        tag5: "CSS",
         image: require("../../assets/Projects/noteTk2.png").default
     },
     {
@@ -37,15 +70,24 @@ export const Projects = [
         description: "This is a downloadable progressive-web-application (PWA) which lets users keep track of expenses or transactions with or without an internet connection.",
         deployedApp: "https://budget-tracker-app-2021.herokuapp.com/",
         github: "https://github.com/kingopara/budget-tracker",
+        tag1: "node.js",
+        tag2: "express.js",
+        tag3: "mongoDB",
+        tag4: "service worker",
+        tag5: "mongoose",
         image: require("../../assets/Projects/budget-tracker.png").default
     },
-    {
-        id:1,
-        title: "Activity Finder",
-        description: "As a user looking for pubs in your local area, pub finder helps you search for local pubs where you could get some relaxation, meet people and have fun with your drinks",
-        deployedApp: "https://kingopara.github.io/activity-finder/",
-        github: "https://github.com/kingopara/activity-finder",
-        image: require("../../assets/Projects/projectPic1.png").default
-    },
+    // {
+    //     id:1,
+    //     title: "Activity Finder",
+    //     description: "As a user looking for pubs in your local area, pub finder helps you search for local pubs where you could get some relaxation, meet people and have fun with your drinks",
+    //     deployedApp: "https://kingopara.github.io/activity-finder/",
+    //     github: "https://github.com/kingopara/activity-finder",
+    //     tag1: "HTML",
+    //     tag2: "CSS",
+    //     tag3: "javaScript",
+
+    //     image: require("../../assets/Projects/projectPic1.png").default
+    // },
     
 ]
